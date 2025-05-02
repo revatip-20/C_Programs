e6239ed3d33cd30b68cb9a75a174ac43194eed47
Revati Ponkshe <107451174+revatip-20@users.noreply.github.com>
2025-05-03 00:07:45 +0530

    Add files via upload

---
+++ Arraydemo2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int Arr[4]={10,20,30,40};
+
+    printf("%d\n",sizeof(Arr));
+
+    printf("%d\n",Arr);
+    printf("%d\n",&Arr);
+
+    printf("%d\n",Arr+1);
+    printf("%d\n",(&Arr)+1);
+
+    return 0;
+}
